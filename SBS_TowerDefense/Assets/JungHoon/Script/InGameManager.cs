14aa31e0086a4636e0d0e693c782c0129340d617
nixeast <eastnix@gmail.com>
2024-06-28 02:26:27 +0900

    Update ingame scene ui

---
+++ SBS_TowerDefense/Assets/JungHoon/Script/InGameManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameManager : MonoBehaviour
+{
+    public GameObject prefabTile;
+    [SerializeField] GameObject objectGameBoard;
+    //GameObject boardTile;
+    List<GameObject> arrBoardTile = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        generateTile(7);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void generateTile(int nSideSize)
+    {
+        float fPosX;
+        float fPosY;
+        float fPosZ;
+        float fAddPos;
+        int nTileIndex;
+
+        nTileIndex = 0;
+        fPosX = -30.0f;
+        fPosY = 30.0f;
+        fPosZ = 0f;
+        fAddPos = 10.0f;
+        for (int i=0; i < nSideSize; i++)
+        {
+            for (int j = 0; j < nSideSize; j++)
+            {
+                GameObject boardTile = Instantiate(prefabTile);
+                boardTile.transform.SetParent(objectGameBoard.transform);
+                arrBoardTile.Add(boardTile);
+                arrBoardTile[nTileIndex].transform.position = new Vector3(fPosX, fPosY, fPosZ);
+                fPosX += fAddPos;
+                nTileIndex++;
+            }
+            fPosX = -30.0f;
+            fPosY -= fAddPos;
+        }
+    }
+}
