5e0aba6d43c28a927a23c68cd430ad60bad3d938
nixeast <eastnix@gmail.com>
2024-06-29 19:42:48 +0900

    Update enemy next location

--- SBS_TowerDefense/Assets/JungHoon/Script/Enemy.cs
+++ SBS_TowerDefense/Assets/JungHoon/Script/Enemy.cs
@@ -11,44 +11,61 @@ public class Enemy : MonoBehaviour
     float fEnemySpeed;
     float fDistanceToTargetLocation;
     bool bShouldMove;
+    Vector3 vec3MoveVector;
 
     // Start is called before the first frame update
     void Start()
     {
-        fEnemySpeed = 0.1f;
+        fEnemySpeed = 0.5f;
         bShouldMove = true;
         currentInGameManager = GameObject.FindObjectOfType<InGameManager>();
         vec3MoveTargetLocation = currentInGameManager.arrBoardTile[0].transform.position;
-        
+        vec3MoveVector = vec3MoveTargetLocation - this.gameObject.transform.position;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(bShouldMove == true)
+        if (bShouldMove == true)
         {
-            moveToTargetLocation(vec3MoveTargetLocation);
+            moveToTargetLocation(vec3MoveVector);
         }
 
         calculateTargetLocationArrive(vec3MoveTargetLocation, out fDistanceToTargetLocation);
-        
-        if(fDistanceToTargetLocation <= 1.0f)
+
+        if (fDistanceToTargetLocation <= 1.0f)
         {
             bShouldMove = false;
         }
 
+        if (bShouldMove == false)
+        {
+            Vector3 vector3Next = currentInGameManager.arrBoardTile[42].transform.position;
+            changeTargetLocation(vector3Next);
+            bShouldMove = true;
+        }
+
     }
 
-    void moveToTargetLocation(Vector3 vec3MoveTargetLocation)
+    void moveToTargetLocation(Vector3 vec3MoveVector)
     {
-        this.gameObject.transform.Translate(vec3MoveTargetLocation * Time.deltaTime * fEnemySpeed);
+        //this.gameObject.transform.Translate(vec3MoveTargetLocation * Time.deltaTime * fEnemySpeed);
+        this.gameObject.transform.position += new Vector3(vec3MoveVector.x * Time.deltaTime * fEnemySpeed, vec3MoveVector.y * Time.deltaTime * fEnemySpeed, 0f);
     }
 
     void calculateTargetLocationArrive(Vector3 vec3MoveTargetLocation, out float fDistanceToTargetLocation)
     {
 
         fDistanceToTargetLocation = Vector2.Distance(this.gameObject.transform.position, vec3MoveTargetLocation);
+        Debug.Log(fDistanceToTargetLocation);
 
+    }
+
+    void changeTargetLocation(Vector3 vec3NextLocation)
+    {
+        vec3MoveTargetLocation = vec3NextLocation;
+        vec3MoveVector = vec3MoveTargetLocation - this.gameObject.transform.position;
     }
 
     
